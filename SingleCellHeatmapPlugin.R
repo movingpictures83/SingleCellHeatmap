b6ed2209c585a6741ee7729ae7f4ed256d1e8616
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-12-19 18:21:06 -0500

    Initial

---
+++ SingleCellHeatmapPlugin.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+source("RIO.R")
+
+input <- function(inputfile) {
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <<- paste(pfix, "/", sep="")
+  }
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+	pdf(outputfile)
+	pbmc.markers <- readRDS(paste(pfix, parameters["rdsfile", 2], sep="/"))
+	pbmc <- readRDS(paste(pfix, parameters["rdsfile2", 2], sep="/"))
+topN <- as.integer(parameters["N", 2])
+
+pbmc.markers %>%
+    group_by(cluster) %>%
+    dplyr::filter(avg_logFC > 1) %>%
+    slice_head(n = topN) %>%
+    ungroup() -> top10
+DoHeatmap(pbmc, features = top10$gene) + NoLegend()
+}
+
+
